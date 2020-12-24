84a1fa87988fd21f45fbe81617e807c2671e8d33
aryashah278 <aryashah97.as@gmail.com>
2020-12-24 17:21:40 -0600

    Tab switching done..

---
+++ front-end/src/shared/Tabs.svelte
@@ -0,0 +1,43 @@
+<script>
+    
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatcher = createEventDispatcher();
+
+    export let items;
+    export let activeItem;
+
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item}
+            <li on:click={() => dispatcher('tabChange', item)}>
+                <div class:active={item === activeItem}>{item}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs{
+        margin-bottom: 40px;
+    }
+    ul{
+        list-style-type: none;
+        display: flex;
+        justify-content: center;
+        padding: 0px;
+    }
+    li{
+        font-size: 18px;
+        margin: 0 16px;
+        color: grey; 
+        cursor: pointer;
+    }
+    .active{
+        color: red;
+        border-bottom: 2px solid red;
+        padding-bottom: 8px;
+    }
+</style>
